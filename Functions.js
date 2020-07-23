5f1471f627e9b80be1747d02d17b3f98a2774d21
mareo1997 <mareo1997@gmail.com>
2020-07-23 11:58:38 -0500

    Commiting changes

--- Functions.js
+++ Functions.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+
+function redirect() {
+    window.open("HomePage.html", "_self");
+}
+
+function Page(page) {
+    if(page == 'about'){
+        window.open("HomePage.html", "_self");
+    } else if (page == 'resume') {
+        window.open("Resume.html", "_self");
+    } else if (page == 'references') {
+        window.open("References.html", "_self");
+    }
+}
